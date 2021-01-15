e2a8c9421a319a4a40bcc2965f4558f1745aa43b
yudha-haris <yudha-haris@ui.ac.id>
2021-01-15 09:10:20 +0700

    menambahkan fitur nama peminjam dan perbaikan penampil status

--- e-perpus.py
+++ e-perpus.py
@@ -1,8 +1,28 @@
 import tkinter
 from tkinter import *
 from tkinter.filedialog import asksaveasfilename, askopenfilename
+from tkinter import scrolledtext
 
-def load_file():
+def load_file_peminjam():
+	file_name = askopenfilename(filetypes=[("All files", "*")])
+	if not file_name:
+		return
+	text_file = open(file_name, 'r', encoding="utf-8")
+	result = text_file.read()
+	global data_pinjam
+	data_pinjam = eval(result)
+	overlay_peminjam.destroy()
+
+def save_file_peminjam():
+	file_name = asksaveasfilename(filetypes=[("All files", "*")])
+	if not file_name:
+		return
+		
+	with open(file_name, "w") as output_file:
+		output_file.write(str(data_pinjam))
+		overlay_peminjam.destroy()  
+
+def load_file_katalog():
 	file_name = askopenfilename(filetypes=[("All files", "*")])
 	if not file_name:
 		return
@@ -12,7 +32,7 @@ def load_file():
 	database = eval(result)
 	overlay_status.destroy()
 
-def save_file():
+def save_file_katalog():
 	file_name = asksaveasfilename(filetypes=[("All files", "*")])
 	if not file_name:
 		return
@@ -28,10 +48,14 @@ def status():
 	overlay_status.geometry("300x300")
 	frame_BOT = Frame(overlay_status)
 	frame_BOT.pack(side=BOTTOM)
+	frame_TOP = Frame(overlay_status)
+	frame_TOP.pack(side=TOP)
+	Button(frame_TOP, text="Katalog", relief=FLAT).pack(side= LEFT, fill=BOTH, expand=YES)
+	Button(frame_TOP, text="Peminjam", command=pinjaman).pack(side=LEFT, fill=BOTH, expand=YES)
 	Label(overlay_status, text="\nSTATUS", font=("Metropolis Black", 15)).pack()
-	Button(frame_BOT, text="SAVE", command=save_file).pack(side=LEFT, pady=10)
+	Button(frame_BOT, text="SAVE", command=save_file_katalog).pack(side=LEFT, pady=10)
 	Label(frame_BOT, text="	").pack(side=LEFT)
-	Button(frame_BOT, text="LOAD", command=load_file).pack(side=LEFT, pady=10)
+	Button(frame_BOT, text="LOAD", command=load_file_katalog).pack(side=LEFT, pady=10)
 
 	# ACTION
 	status_data = []
@@ -41,13 +65,57 @@ def status():
 			if i % 6 == 0:
 				status_data.append(f"{y[i]}, stock: {y[i+5]}")
 	if status_data == []:
-		result_status_data = "Belum ada buku maupun rak!"
+		result_status_data = "Belum ada buku maupun rak!\nsilakan load database manual"
 	else:
 		result_status_data = "\n".join(status_data)
-	Label(overlay_status, text=result_status_data).pack() 
+	
+	
+	global result_katalog
+	result_katalog = scrolledtext.ScrolledText(overlay_status,  width = 40,  height = 10, font=("Times New Roman",10))
+	result_katalog.pack()
+	result_katalog.insert(INSERT, result_status_data)
+	result_katalog.configure(state=DISABLED)
+	
+def pinjaman():
+	# GUI
+	overlay_status.destroy()
+	global overlay_peminjam
+	overlay_peminjam = Toplevel()
+	overlay_peminjam.geometry("300x300")
+	frame_BOT = Frame(overlay_peminjam)
+	frame_BOT.pack(side=BOTTOM)
+	frame_TOP = Frame(overlay_peminjam)
+	frame_TOP.pack(side=TOP)
+	Button(frame_TOP, text="Katalog", command=oper).pack(side= LEFT, fill=BOTH, expand=YES)
+	Button(frame_TOP, text="Peminjam", relief=FLAT).pack(side=LEFT, fill=BOTH, expand=YES)
+	Label(overlay_peminjam, text="\nSTATUS", font=("Metropolis Black", 15)).pack()
+	Button(frame_BOT, text="SAVE", command=save_file_peminjam).pack(side=LEFT, pady=10)
+	Label(frame_BOT, text="	").pack(side=LEFT)
+	Button(frame_BOT, text="LOAD", command=load_file_peminjam).pack(side=LEFT, pady=10)
+
+	# ACTION
+	status_data = []
+	for x,y in data_pinjam.items():
+		status_data.append(f"___{x}___")
+		for i in range(len(y)):
+			if i % 6 == 0:
+				status_data.append(f"{y[i]}, banyak: {y[i+1]}")
+	if status_data == []:
+		result_status_data = "Belum ada buku dipinjam!\nsilakan load database manual"
+	else:
+		result_status_data = "\n".join(status_data)
+	result_peminjam = scrolledtext.ScrolledText(overlay_peminjam,  width = 40,  height = 10, font=("Times New Roman",10))
+	result_peminjam.pack()
+	result_peminjam.insert(INSERT, result_status_data)
+	result_peminjam.configure(state=DISABLED)
+
+def oper():
+	overlay_peminjam.destroy()
+	status()
 
 database = {}
 data_pinjam = {}
+
 class Underground(Tk):
 	# Implementasi ini saya dapatkan dari internet dengan keyword "Tkinter multiple frame"
 	# diambil dari beberapa tautan, di antaranya
@@ -78,6 +146,7 @@ class Underground(Tk):
 		self.frames["sub_rak"] = sub_rak(master=container, controller=self)
 		self.frames["rak_menu"] = rak_menu(master=container, controller=self)
 		self.frames["buku_menu"] = buku_menu(master=container, controller=self)
+		self.frames["return_buku"] = return_buku(master=container, controller=self)
 		
 		self.frames["Home"].grid(row=0, column=0, sticky="NSEW")
 		self.frames["login"].grid(row=0, column=0, sticky="NSEW")
@@ -89,6 +158,7 @@ class Underground(Tk):
 		self.frames["sub_rak"].grid(row=0, column=0, sticky="NSEW")
 		self.frames["rak_menu"].grid(row=0, column=0, sticky="NSEW")
 		self.frames["buku_menu"].grid(row=0, column=0, sticky="NSEW")
+		self.frames["return_buku"].grid(row=0, column=0, sticky="NSEW")
 
 		# menampilkan halaman
 		self.show_frame("Home_Front")
@@ -319,12 +389,14 @@ class buku_menu(Frame):
 		Label(self, text="Menu Buku", font=("Metropolis Black", 20)).grid(row=1,column=0,sticky=E, padx=120, pady=20)
 
 		# BUTTON
-		Button(self, text=" TAMBAHKAN BUKU ", command=lambda: self.controller.show_frame("add_buku"), relief=SOLID, font=("Metropolis", 12, "bold")).grid(row=2,
-		 column=0, padx= 20, pady=2, rowspan=2)
-		Button(self, text="  PINJAMKAN BUKU  ", command=lambda: self.controller.show_frame("sub_buku"), relief=SOLID, font=("Metropolis", 12, "bold")).grid(row=4,
-		 column=0, padx= 20, pady=10, rowspan=2)
-		Button(self, text="TAMBAH STOK BUKU", command=lambda: self.controller.show_frame("add_stok_buku"), relief=SOLID, font=("Metropolis", 12, "bold")).grid(row=6,
-		 column=0, padx= 20, pady=2, rowspan=2)
+		Button(self, text="   TAMBAHKAN BUKU   ", command=lambda: self.controller.show_frame("add_buku"), relief=SOLID, font=("Metropolis", 12, "bold")).grid(row=2,
+		 column=0, padx= 10, pady=10, rowspan=2)
+		Button(self, text="    PINJAMKAN BUKU    ", command=lambda: self.controller.show_frame("sub_buku"), relief=SOLID, font=("Metropolis", 12, "bold")).grid(row=4,
+		 column=0, padx= 10, pady=10, rowspan=2)
+		Button(self, text="  TAMBAH STOK BUKU  ", command=lambda: self.controller.show_frame("add_stok_buku"), relief=SOLID, font=("Metropolis", 12, "bold")).grid(row=6,
+		 column=0, padx= 10, pady=10, rowspan=2)
+		Button(self, text="PENGEMBALIAN BUKU", command=lambda: self.controller.show_frame("return_buku"), relief=SOLID, font=("Metropolis", 12, "bold")).grid(row=8,
+		 column=0, padx= 10, pady=10, rowspan=2)
 
 class add_rak(Frame):
 	def __init__(self, master, controller):
@@ -477,7 +549,7 @@ class sub_buku(Frame):
 		self.peminjam = StringVar()
 		self.nama_buku = StringVar()
 		self.jumlah = StringVar()
-		self.e0 = Entry(self, width="30", textvariable= self.nama_buku).grid(row=2, column=0,sticky = E, columnspan=2)
+		self.e0 = Entry(self, width="30", textvariable= self.peminjam).grid(row=2, column=0,sticky = E, columnspan=2)
 		self.e1 = Entry(self, width="30", textvariable= self.nama_buku).grid(row=3, column=0,sticky = E, columnspan=2)
 		self.e2 = Entry(self, width="30", textvariable= self.jumlah).grid(row=4, column=0,sticky = E, columnspan=2)
 
@@ -502,10 +574,82 @@ class sub_buku(Frame):
 					else:
 						y[i+5] = str(int(y[i+5]) - int(self.jumlah.get()))
 						Label(overlay, text=f"Buku dengan nama {self.nama_buku.get()}\ndipinjamkan sebanyak {self.jumlah.get()}").pack()
+						if self.peminjam.get() in data_pinjam.keys():
+							if self.nama_buku.get() in data_pinjam[self.peminjam.get()]:
+								indeks = data_pinjam[self.peminjam.get()].index(self.nama_buku.get())
+								data_pinjam[self.peminjam.get()][indeks+1] = data_pinjam[self.peminjam.get()][indeks+1] + int(self.jumlah.get())
+							else:
+								data_pinjam[self.peminjam.get()].append(self.nama_buku.get())
+								data_pinjam[self.peminjam.get()].append(self.jumlah.get())
+						else:
+							data_pinjam.update({self.peminjam.get():[self.nama_buku.get(),self.jumlah.get()]})
 						config = 1
 						break
 		if config == 0:
 			Label(overlay, text=f"Buku dengan nama {self.nama_buku.get()} tidak ada!").pack()
+
+class return_buku(Frame):
+	# Constructor untuk class add_rak()
+	def __init__(self, master, controller):
+		self.controller = controller
+		self.master = master
+		Frame.__init__(self, master)
+		self.make_widget()
+
+	def make_widget(self):
+		# Membuat Frame
+		self.frame_TOP = Frame(self)
+		self.frame_TOP.grid(row=0, sticky=W)
+		self.frame_TOP_Left = Frame(self)
+		self.frame_TOP_Left.grid(row=0, sticky=E)
+
+		# Kumpulan Button, Entry, dan Label
+
+		# Navigasi
+		Button(self.frame_TOP, text="HOME", command=lambda: self.controller.show_frame("Home"), relief=FLAT, fg="black", font=("Metropolis", 10)).grid(row=0, column=0)
+		Button(self.frame_TOP, text="RAK",command=lambda: self.controller.show_frame("rak_menu"),relief=FLAT,fg="black", font=("Metropolis", 10)).grid(row=0, column=1)
+		Button(self.frame_TOP, text="BUKU", command=None, bg="#DFE3EE",fg="black", font=("Metropolis", 10)).grid(row=0, column=2)
+		Button(self.frame_TOP, text="STATUS", command=status, relief=FLAT, fg="black", font=("Metropolis", 10)).grid(row=0, column=3)
+		
+		# LABEL
+		Label(self, text="Pengembalian", font=("Metropolis Black", 20)).grid(row=1,column=0, columnspan=2, padx=60, pady=20)
+		Label(self, text="PEMINJAM	").grid(row=2, sticky=W, padx= 20)
+		Label(self, text="NAMA BUKU	").grid(row=3, sticky=W, padx= 20)
+		Label(self, text="JUMLAH	").grid(row=4, sticky=W, padx= 20)	
+
+		# ENTRY
+		self.peminjam = StringVar()
+		self.nama_buku = StringVar()
+		self.jumlah = StringVar()
+		self.e0 = Entry(self, width="30", textvariable= self.peminjam).grid(row=2, column=0,sticky = E, columnspan=2)
+		self.e1 = Entry(self, width="30", textvariable= self.nama_buku).grid(row=3, column=0,sticky = E, columnspan=2)
+		self.e2 = Entry(self, width="30", textvariable= self.jumlah).grid(row=4, column=0,sticky = E, columnspan=2)
+
+		# BUTTON
+		Button(self, text="<< BACK", command=lambda: self.controller.show_frame("buku_menu"), relief=FLAT, font=("Metropolis", 10), bg="#003B7A", fg="white").grid(row=8, column=0, sticky=W, padx= 20, pady=50)
+		Button(self, text="KEMBALIKAN", command=self.kembalian_buku, relief=FLAT, font=("Metropolis", 10), bg="#003B7A", fg="white").grid(row=8, column=1, sticky=E, padx= 20, pady=50)
+
+	def kembalian_buku(self):
+		# GUI
+		overlay = Toplevel(self)
+		overlay.geometry("300x50")
+
+		# ACTION
+		if self.peminjam.get() in data_pinjam.keys():
+			if self.nama_buku.get() in data_pinjam[self.peminjam.get()]:
+				indeks = data_pinjam[self.peminjam.get()].index(self.nama_buku.get())
+				data_pinjam[self.peminjam.get()][indeks+1] = data_pinjam[self.peminjam.get()][indeks+1] - int(self.jumlah.get())
+				for x,y in database.items():
+					for i in range(len(y)):
+						if self.nama_buku.get() == y[i]:
+							y[i+5] = str(int(y[i+5]) + int(self.jumlah.get()))
+							Label(overlay, text=f"Buku dengan nama {self.nama_buku.get()}\nsudah dikembalikan oleh {self.peminjam.get()}").pack()
+							print(data_pinjam)
+							break
+			else:
+				Label(overlay, text=f"Buku dengan nama {self.nama_buku.get()}\n tidak ada atau tidak dipinjam!").pack()
+		else:
+			Label(overlay, text=f"Peminjam atas nama {self.peminjam.get()} tidak ada!").pack()
 
 class add_buku(Frame):
 	# Constructor __init__ untuk class add_buku
@@ -617,9 +761,9 @@ class add_stok_buku(Frame):
 
 		# BUTTON
 		Button(self, text="<< BACK", command=lambda: self.controller.show_frame("buku_menu"), relief=FLAT, font=("Metropolis", 10), bg="#003B7A", fg="white").grid(row=8, column=0, sticky=W, padx= 20, pady=50)
-		Button(self, text="TAMBAH", command=self.pinjamkan_buku, relief=FLAT, font=("Metropolis", 10), bg="#003B7A", fg="white").grid(row=8, column=1, sticky=E, padx= 20, pady=50)
+		Button(self, text="TAMBAH", command=self.tambah_stok, relief=FLAT, font=("Metropolis", 10), bg="#003B7A", fg="white").grid(row=8, column=1, sticky=E, padx= 20, pady=50)
 
-	def pinjamkan_buku(self):
+	def tambah_stok(self):
 		# GUI
 		overlay = Toplevel(self)
 		overlay.geometry("300x50")
